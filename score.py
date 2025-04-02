a2bc5895afa742fc4b85442bca90bba249fd17b9
Pamecant <136908623+Pamecant@users.noreply.github.com>
2025-04-01 18:21:32 -0600

    Add files via upload

---
+++ score.py
@@ -0,0 +1,23 @@
+
+import json
+import joblib
+import numpy as np
+import pandas as pd
+from azureml.core.model import Model
+
+def init():
+  global model
+  model_path = Model.get_model_path('model')
+  model = joblib.load(model_path)
+
+
+def run(raw_data):
+  try: ## Try la predicción.
+    data = json.loads(raw_data)['data'][0]
+    data = pd.DataFrame(data)
+
+    result = model.predict(data).tolist()
+
+    return json.dumps(result)
+  except Exception as e:
+    return json.dumps(str(e))
